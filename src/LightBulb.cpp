4e27b2ffb80805f8b8c66cffe9652c7feddd06e5
AndrewBorisov81 <ibobojohn81@gmail.com>
2022-09-26 21:42:52 +0300

    Added LightBulb.cpp

---
+++ src/LightBulb.cpp
@@ -0,0 +1,25 @@
+#include "LightBulb.h"
+
+LightBulb::LightBulb() {
+
+}
+
+LightBulb::LightBulb(ColorLightBulb color) : m_color{color} {
+    
+}
+
+LightBulb::~LightBulb() {
+
+}
+
+void LightBulb::On() { 
+    std::cout << colour_name[static_cast<int>(m_color)] << " LightBulb On!" << '\n'; 
+}
+
+void LightBulb::Off() { 
+    std::cout << colour_name[static_cast<int>(m_color)] << " LightBulb Off!" << '\n'; 
+}
+
+std::unique_ptr<ILightBulb> LightBulb::clone() const {
+    return std::make_unique<LightBulb>(*this);
+}
