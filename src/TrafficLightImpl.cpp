f5936ea020f160d239227cacc77d40080e331384
AndrewBorisov81 <ibobojohn81@gmail.com>
2022-10-07 10:07:23 +0300

    Update name

--- src/TrafficLightImpl.cpp
+++ src/TrafficLightImpl.cpp
@@ -10,16 +10,16 @@ TrafficLightImpl::~TrafficLightImpl() {
 
 void TripleTrafficLightImpl::triple_allow(std::vector<std::unique_ptr<ILightBulb>> lightBulbs) {
     if(lightBulbs.size() == 3) {
-        std::unique_ptr<ILightBulb> yellowlightBulb{std::move(lightBulbs.at(2))};
-        yellowlightBulb->On();
+        std::unique_ptr<ILightBulb> yellowLightBulb{std::move(lightBulbs.at(2))};
+        yellowLightBulb->On();
     }
     std::cout << "Triple trafficLight triple_allow" << "\n";
 }
 
 void TripleTrafficLightImpl::triple_disallow(std::vector<std::unique_ptr<ILightBulb>> lightBulbs) {
      if(lightBulbs.size() == 3) {
-        std::unique_ptr<ILightBulb> yellowlightBulb{std::move(lightBulbs.at(2))};
-        yellowlightBulb->Off();
+        std::unique_ptr<ILightBulb> yellowLightBulb{std::move(lightBulbs.at(2))};
+        yellowLightBulb->Off();
     }
     std::cout << "Triple trafficLight triple_disallow" << "\n";
 }
